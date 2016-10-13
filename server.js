7726e96a3ae034844ff15dfc0591289bd8a7ea24
akira <syuchihin@hotmail.com>
2016-10-13 14:21:55 +0900

    one click connecting

--- server.js
+++ server.js
@@ -1,21 +1,34 @@
 var fs = require('fs');
-var http = require('http');
 var https = require('https');
-var privateKey  = fs.readFileSync('./server.key', 'utf8');
-var certificate = fs.readFileSync('./server.crt', 'utf8');
-
-var credentials = {key: privateKey, cert: certificate};
 var express = require('express');
 var app = express();
 
-// your express configuration here
 
-var httpServer = http.createServer(app);
-var httpsServer = https.createServer(credentials, app);
+var privateKey  = fs.readFileSync('./server.key', 'utf8');
+var certificate = fs.readFileSync('./server.crt', 'utf8');
+var credentials = {key: privateKey, cert: certificate};
+
+
+var server = https.createServer(credentials, app);
+
+var io = require('socket.io')(server);
 
 app.get('/', function(req, res){
 	res.sendfile('static/index.html');
 })
 
-httpServer.listen(8080);
-httpsServer.listen(8443);
+io.on('connection', function (socket) {
+	// console.log('a user connected');
+	// socket.on('disconnect', function () {
+	// 	console.log('user disconnected');
+	// });
+	socket.on('message', function (message) {
+		socket.broadcast.emit('message', message);
+	});
+
+	socket.on('disconnect', function () {
+		socket.broadcast.emit('user disconnected');
+	});
+});
+
+server.listen(3000);
